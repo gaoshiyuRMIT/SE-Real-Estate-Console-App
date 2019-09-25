91e97874a8cf8c37b9b186fbeace8ebfb6689309
rmit-s3748845-ke-xu <54495062+rmit-s3748845-ke-xu@users.noreply.github.com>
2019-09-25 17:42:16 +1000

    Revert "someSalaryThing"
    This reverts commit ebb0241491345dcf38da8bebd8fafad5a40060c0.

--- test/salary/testSalary.java
+++ test/salary/testSalary.java
@@ -1,50 +1,46 @@
 package salary;
-
 import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.*;
-import java.time.*;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 
-import se.*;
-import exception.*;
-import user.customer.*;
 import user.employee.*;
-import consts.*;
-import org.junit.Test;
-
 
 public class testSalary {
 	private Employee e1;
+	/*
+	 * check if entered work hours is 1.valid(not minus ) and 2.not null
+	 */
+	@Test
+	public void testWorkHours() throws Exception{
+		assertNotNull(e1.getHours());
+		assertTrue( e1.getHours() > 0);
 
+	}	
+	
 	/*
 	 * check if Part-timers are paid a salary that is proportional to the hours they worked
 	 * SalesConsultant have extra 40% of commission
 	 */
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testPaidSalary() throws Exception{
-		PropertyManager pm1 = new PropertyManager("pm1@gmail.com","pswd");
-		BranchManager bm1 = new BranchManager("bm1@gmail.com","pswd");
-		SalesConsultant sc1 = new SalesConsultant("sc1@gmail.com","pswd");
-		assertEquals(bm1.getBaseSalary(), bm1.getHours()*Employee.getHourlySalary()+0.4*commissionReceived);
-		
+		PropertyManager pm1 = new PropertyManager("pm1@gmail.com","pswd",1);
+		BranchManager bm1 = new BranchManager("bm1@gmail.com","pswd",1);
+		SalesConsultant sc1 = new SalesConsultant("sc1@gmail.com","pswd",1);
+		//assume hoursalary 20 and commissionreceived=1000
+		assertEquals(bm1.getSalary(), bm1.getSalary()+0.4*1000);
+	}	
 	/*
 	 * check if the status change 
 	 * when they get the branch manager approval 
 	 */
 	 @Test
 	 public void testPartTimeSalary() throws Exception{
-		Employee pt1 = new Employee("pt1@gmail.com","pswd",35);
+		PartTimer pt1 = new PartTimer("pt1@gmail.com","pswd",35);
 		assertTrue(pt1.getSalary().isApproved());
 		}
 
